d8f9a58533eef506c96aad1721cd44a579afe60c
victorhenriqc <vic.1601@outlook.com>
2022-06-12 15:36:44 -0300

    add a query email im get jogadores

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './interfaces/jogador.interface';
 import { v4 as uuidV4 } from 'uuid';
@@ -25,6 +25,16 @@ export class JogadoresService {
 
   async consultarTodosJogadores(): Promise<Jogador[]> {
     return await this.jogadores;
+  }
+
+  async consultarJogadoresPorEmail(email: string): Promise<Jogador> {
+    const jogadorEncontrado = await this.jogadores.find(
+      (jogador) => jogador.email === email,
+    );
+    if (!jogadorEncontrado) {
+      throw new NotFoundException('Jogador não encontrado');
+    }
+    return jogadorEncontrado;
   }
 
   private criar(criaJogadorDto: CriarJogadorDto): void {
