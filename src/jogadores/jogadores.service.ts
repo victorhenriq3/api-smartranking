d862c31dc97aa7a99566d8960a210634926b0372
victorhenriqc <vic.1601@outlook.com>
2022-06-26 16:44:49 -0300

    add libs classes transform and validator

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './interfaces/jogador.interface';
 
