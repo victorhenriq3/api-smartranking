54cc16a5292069e6b8f937f6ea63524e4e742f1c
victorhenriqc <vic.1601@outlook.com>
2022-07-09 20:43:31 -0300

    adicioando consulta de jogador na categoria

--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { JogadoresService } from 'src/jogadores/jogadores.service';
 import { AtualizarCategoriaDto } from './dtos/atualizar-categoria.dto';
 import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 import { Categoria } from './interface/categoria.interface';
@@ -13,6 +14,7 @@ import { Categoria } from './interface/categoria.interface';
 export class CategoriasService {
   constructor(
     @InjectModel('Categoria') private readonly categoriaModel: Model<Categoria>,
+    private readonly jogadoresService: JogadoresService,
   ) {}
 
   async criarCategoria(
@@ -79,6 +81,14 @@ export class CategoriasService {
     if (!categoriaEncontrada) {
       throw new NotFoundException('Categoria não encontrada!');
     }
+
+    const jogadorCadastradoCategoria = await this.categoriaModel
+      .find({ categoria })
+      .where('jogadores')
+      .in(idJogador)
+      .exec();
+
+    await this.jogadoresService.consultarJogadorPeloId(idJogador);
 
     categoriaEncontrada.jogadores.push(idJogador);
 
